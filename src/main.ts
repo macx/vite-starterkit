a3c401513588c368237b7ca7e6bf661da9d195c8
David Maciejewski <macx@macx.io>
2023-09-21 12:11:33 +0200

    Switch to Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './css/main.scss';
-import starterkitLogo from './images/vite-starterkit.svg';
-import packageJson from '../package.json';
+import starterkitLogo from './assets/images/vite-starterkit.svg';
+import { version } from '../package.json';
 
 /* prettier-ignore */
 console.log('%c┌─────────────────────────────────────────┐\n  VITE STARTERKIT                          \n  https://github.com/macx/vite-starterkit  \n└─────────────────────────────────────────┘', 'background: black; color: limegreen');
 
-document.querySelector('.logo').innerHTML = `<img src="${starterkitLogo}">`;
-document.querySelector('.version').innerHTML = packageJson.version;
+document.querySelector<HTMLDivElement>('.logo')!.innerHTML = `<img src="${starterkitLogo}">`;
+document.querySelector<HTMLDivElement>('.version')!.innerHTML = `v ${version}`;
